384e47183ff9e569abf1acc07144aec5262eeb1f
Leandro Souza Oliveira <leandro.oliveirasouza1@gmail.com>
2022-09-30 15:26:50 -0300

    User insertion with POST

--- src/main/java/com/leandrosouza/workshopmongo/services/UserService.java
+++ src/main/java/com/leandrosouza/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.leandrosouza.workshopmongo.domain.User;
+import com.leandrosouza.workshopmongo.dto.UserDTO;
 import com.leandrosouza.workshopmongo.repository.UserRepository;
 import com.leandrosouza.workshopmongo.services.excepition.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
